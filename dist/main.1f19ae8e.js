61f9bd148f72503d5d368f91e9338e926dc227ee
Jen Pike <jenn_e_fur16@hotmail.com>
2021-07-17 17:37:17 -0400

    removed code not working right now

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -150,7 +150,7 @@ $(function () {
     $(".projectExample .active").removeClass("active").addClass("hidden").prev().removeClass("hidden").addClass("active");
   });
 });
-},{}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62029" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62268" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -354,5 +354,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
+},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
 //# sourceMappingURL=/main.1f19ae8e.js.map
