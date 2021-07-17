f1c15dfa938c9c264e0df300aec2147f457f11a6
Jen Pike <jenn_e_fur16@hotmail.com>
2021-07-17 17:03:58 -0400

    added new icons, updated styles and colours

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 $(function () {
-    // code to hide and show my hamburger menu
-    $('.hamburger').on('click', function () {
-        $('.showNav').toggleClass('show');
-    });
+  // code to hide and show my hamburger menu
+  $(".hamburger").on("click", function () {
+    $(".showNav").toggleClass("show");
+  });
 
-    $('.exitResponsive').on('click', function() {
-        $('.showNav').toggleClass('show');
-    });
+  $(".exitResponsive").on("click", function () {
+    $(".showNav").toggleClass("show");
+  });
 
-    // Code to switch between projects in my project gallery section
-    $('.right').on('click', function(){
-        const $next = $('.projectExample .active').next();
+  // Code to switch between projects in my project gallery section
+  $(".right").on("click", function () {
+    const $next = $(".projectExample .active").next();
 
-        if ($next.length === 0) {
-            $('.projectExample .active').removeClass('active').addClass('hidden');
-            $('.projectExample .project1').removeClass('hidden').addClass('active');
-            return; //do nothing
-        }
-        $('.projectExample .active').removeClass('active').addClass('hidden').next().removeClass('hidden').addClass('active');
-    })
+    if ($next.length === 0) {
+      $(".projectExample .active").removeClass("active").addClass("hidden");
+      $(".projectExample .project1").removeClass("hidden").addClass("active");
+      return; //do nothing
+    }
+    $(".projectExample .active")
+      .removeClass("active")
+      .addClass("hidden")
+      .next()
+      .removeClass("hidden")
+      .addClass("active");
+  });
 
-    $('.left').on('click', function(){
-        const $prev = $('.projectExample .active').prev();
+  $(".left").on("click", function () {
+    const $prev = $(".projectExample .active").prev();
 
-        if ($prev.length === 0) {
-            $('.projectExample .active').removeClass('active').addClass('hidden');
-            $('.projectExample .project4').removeClass('hidden').addClass('active');
-            return; //do nothing
-        }
-        $('.projectExample .active').removeClass('active').addClass('hidden').prev().removeClass('hidden').addClass('active');
-    })
-
-
+    if ($prev.length === 0) {
+      $(".projectExample .active").removeClass("active").addClass("hidden");
+      $(".projectExample .project4").removeClass("hidden").addClass("active");
+      return; //do nothing
+    }
+    $(".projectExample .active")
+      .removeClass("active")
+      .addClass("hidden")
+      .prev()
+      .removeClass("hidden")
+      .addClass("active");
+  });
 });
